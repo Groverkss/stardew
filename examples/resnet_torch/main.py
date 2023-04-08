2ccbcf5890a099cccab99370d9ef630dfc5595a8
Groverkss <groverkss@gmail.com>
2023-04-08 23:05:20 +0530

    Use torch_mlir.dynamo

--- examples/resnet_torch/main.py
+++ examples/resnet_torch/main.py
@@ -6,24 +6,9 @@ from torch._decomp import get_decompositions
 from torchvision.models import resnet50
 
 import torch_mlir
+from torch_mlir.dynamo import make_simple_dynamo_backend
 
 from typing import List, Optional
-
-def default_decompositions():
-    return get_decompositions(
-        [
-            torch.ops.aten.embedding_dense_backward,
-            torch.ops.aten.native_layer_norm_backward,
-            torch.ops.aten.slice_backward,
-            torch.ops.aten.select_backward,
-            torch.ops.aten.norm.ScalarOpt_dim,
-            torch.ops.aten.native_group_norm,
-            torch.ops.aten.upsample_bilinear2d.vec,
-            torch.ops.aten.split.Tensor,
-            torch.ops.aten.split_with_sizes,
-            torch.ops.aten.native_batch_norm,
-        ]
-    )
 
 def _returns_nothing(fx_g: torch.fx.GraphModule) -> bool:
     for node in fx_g.graph.nodes:
@@ -37,52 +22,21 @@ def _returns_nothing(fx_g: torch.fx.GraphModule) -> bool:
     return False
 
 
-def _unwrap_single_tuple_return(fx_g: torch.fx.GraphModule) -> bool:
-    """
-    Replace tuple with tuple element in functions that return one-element tuples.
-    Returns true if an unwrapping took place, and false otherwise.
-    """
-    unwrapped_tuple = False
-    for node in fx_g.graph.nodes:
-        if node.op == "output":
-            assert (
-                len(node.args) == 1
-            ), "Output node must have a single argument"
-            node_arg = node.args[0]
-            if isinstance(node_arg, tuple):
-                if len(node_arg) == 1:
-                    node.args = (node_arg[0],)
-                    unwrapped_tuple = True
-                    break
-
-    if unwrapped_tuple:
-        fx_g.graph.lint()
-        fx_g.recompile()
-    return unwrapped_tuple
-
-
 def make_compiler():
     def compiler(
         fx_graph: torch.fx.GraphModule,
         example_inputs: List[torch.Tensor],
     ):
         """Compile GraphModule using torch-mlir."""
-        print("Compiling graph...")
 
         if _returns_nothing(fx_graph):
             return fx_graph
-
-        was_unwrapped = _unwrap_single_tuple_return(fx_graph)
-        fx_graph = make_fx(
-            fx_graph, decomposition_table=default_decompositions()
-        )(*example_inputs)
-        strip_overloads(fx_graph)
 
         torch_mlir_module = torch_mlir.compile(fx_graph,
             example_inputs,
             output_type=torch_mlir.OutputType.TORCH,
         )
-        print("\n\ntorch-mlir backend contract graph:")
+
         print(torch_mlir_module)
 
         def forward(*inputs):
@@ -94,7 +48,7 @@ def make_compiler():
 
 dynamo.reset()
 
-@dynamo.optimize(make_compiler())
+@dynamo.optimize(make_simple_dynamo_backend(make_compiler()))
 def resnet(t):
     model = resnet50()
     with torch.no_grad():
