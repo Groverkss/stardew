739dc3f9779b6592d4075820e6086b6eb1582b55
Groverkss <groverkss@gmail.com>
2023-04-21 15:17:22 +0530

    Fix output for runtime backend

--- examples/matmul_torch/main.py
+++ examples/matmul_torch/main.py
@@ -3,15 +3,11 @@ import torch._dynamo as dynamo
 
 from stardew.frontend import torch_compiler
 
-dynamo.reset()
-
-
 @dynamo.optimize(torch_compiler(output_type="compiled_fn"))
 def matmul(a, b):
     return torch.matmul(a, b)
 
-
-a = torch.randn(3, 3)
-b = torch.randn(3, 3)
-c = matmul(a, b)
-print(c)
+a = torch.randn(4, 4)
+b = torch.randn(4, 4)
+out = matmul(a, b)
+print(out)
--- stardew/backend/torch_mlir_backend.py
+++ stardew/backend/torch_mlir_backend.py
@@ -1,8 +1,12 @@
 from torch_mlir_e2e_test.linalg_on_tensors_backends import refbackend
+import torch
 
 def torch_mlir_backend(linalg_module, inputs):
     backend = refbackend.RefBackendLinalgOnTensorsBackend()
     compiled = backend.compile(linalg_module)
     jit_module = backend.load(compiled)
     np_inputs = [x.numpy() for x in inputs]
-    return jit_module.forward(*np_inputs)
+    out = jit_module.forward(*np_inputs)
+    if not isinstance(out, tuple):
+        return torch.from_numpy(out)
+    return [torch.from_numpy(x) for x in out]
