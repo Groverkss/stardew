0c3942223d00709a0bd6cdb4da1c741703d92b40
Groverkss <groverkss@gmail.com>
2023-06-22 17:42:44 +0530

    Update examples

--- examples/alexnet_torch/main.py
+++ examples/alexnet_torch/main.py
@@ -1,52 +1,12 @@
+from stardew.torch import torch_compiler
+
 import torch
-import torch._dynamo as dynamo
+from torchvision.models import alexnet
 
-from stardew.frontend import torch_compiler
-
-from torch import nn
-
-class AlexNet(nn.Module):
-    def __init__(self, num_classes: int = 1000, dropout: float = 0.5) -> None:
-        super().__init__()
-        self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(64, 192, kernel_size=5, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(192, 384, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(384, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-        )
-        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
-        self.classifier = nn.Sequential(
-            nn.Dropout(p=dropout),
-            nn.Linear(256 * 6 * 6, 4096),
-            nn.ReLU(inplace=True),
-            nn.Dropout(p=dropout),
-            nn.Linear(4096, 4096),
-            nn.ReLU(inplace=True),
-            nn.Linear(4096, num_classes),
-        )
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.features(x)
-        x = self.avgpool(x)
-        x = torch.flatten(x, 1)
-        x = self.classifier(x)
-        return x
-
-model = AlexNet()
-model.eval()
-
-@dynamo.optimize(torch_compiler(output_type="input_ir"))
-def fun(x):
-    return model(x)
+model = alexnet()
 
 x = torch.randn(1, 3, 224, 224)
-fun(x)
+
+@torch_compiler(example=[x])
+def fun(x):
+    return model(x)[0]
